eb33a00ebbd54172abcc08719eaa03986db2628f
PragyaAgarwal25 <pragyapihu2@gmail.com>
2026-02-02 14:48:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+//Q1
+//const fruit= ["apple","mango","guava","cherry","banana"]
+//const reverse=[];
+//while(fruit.length>0){
+  //  reverse.push(fruit.pop())
+//}
+//console.log(reverse)
+//Q2
+//let arr=[5,-7,12,-6,-13,15,2]
+//let positive=[];
+//while(arr.length>0){
+  //  let val=arr.shift();
+    //if(val>0){
+      //  positive.push(val);
+    //}
+//}
+//console.log(positive)
+
+
+//Q3
+//function isPalindrome(str) {
+    //let j = str.length - 1
+    //for (let i = 0; i < str.length / 2; i++) {
+      //  if (str[i] != str[j]) {
+        //    return false;
+        //}
+        //j--;
+    //}
+    //return true;
+//}
+
+//let str1 = "racecar";
+//let str2 = "nitin";
+//let str3 = "Rama";
+
+//console.log(isPalindrome(str1));
+//console.log(isPalindrome(str2));
+//console.log(isPalindrome(str3));
+
